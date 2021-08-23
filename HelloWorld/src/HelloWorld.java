3a3cd00d153b33c741d58404f0622626f21ff131
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-23 16:58:05 +0900

    main2 メソッドの名前を main に名前を変更した。クラス内に main メソッドがないと実行できないので。

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,8 +1,9 @@
 
 public class HelloWorld {
-    public static void main2(String [] args) {
+    public static void main(String [] args) {
         System.out.println("Hello World");
         System.out.println("name: 山田 太郎");
         System.out.println("鳥取県");
+        System.out.println("Tottori");
     }
 }
