e7595e6b3a2e41991fd649ad13c421fdf3bd094b
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-23 16:39:03 +0900

    自分で書いたHello world.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+    public static void main2(String [] args) {
+        System.out.println("Hello World");
+        System.out.println("name: 山田 太郎");
+        System.out.println("鳥取県");
+    }
+}
