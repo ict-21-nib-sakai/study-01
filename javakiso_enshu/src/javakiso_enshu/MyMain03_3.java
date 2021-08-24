5370abe0ab31d459591de6f793dea97c14f15970
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-24 16:23:37 +0900

    演習3.3 配列の定義と各要素への代入。

---
+++ javakiso_enshu/src/javakiso_enshu/MyMain03_3.java
@@ -0,0 +1,11 @@
+package javakiso_enshu;
+
+public class MyMain03_3 {
+    public static void main(String[] args) {
+        int [] data = {100, 200, 300};
+        System.out.println("array[0] = " + data[0]);
+        System.out.println("array[1] = " + data[1]);
+        System.out.println("array[2] = " + data[2]);
+        System.out.println("arrayの要素数: " + data.length);
+    }
+}
