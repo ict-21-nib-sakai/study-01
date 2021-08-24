c509bb0646be05053b9ffd8f36b850d5a9f50a48
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-24 09:51:49 +0900

    演習2.1

---
+++ javakiso_enshu/src/javakiso_enshu/MyMain02_1.java
@@ -0,0 +1,13 @@
+package javakiso_enshu;
+
+public class MyMain02_1 {
+    public static void main(String[] args) {
+        System.out.println("こんにちは私の名前は【自分の名前】です。");
+        System.out.println("■年齢");
+        System.out.println("【自分の年齢】");
+        System.out.println("■趣味");
+        System.out.println("【自分の趣味】");
+        System.out.println("■好きな食べ物");
+        System.out.println("【自分の好きな食べ物】");
+    }
+}
