45a21cfb525b38f572769d99848bd3d9b97407d7
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-25 13:38:18 +0900

    3.15.2 引数あり、戻り値なしのパターン。複数の引数がある場合。

--- HelloWorld/src/SampleClass.java
+++ HelloWorld/src/SampleClass.java
@@ -2,6 +2,7 @@ public class SampleClass {
     public static void main(String[] args) {
         sayHelloWorld();
         sayHelloName("Jane Doe");
+        sayNameAndAge("Jane Doe", 20);
     }
 
     public static void sayHelloWorld() {
@@ -13,5 +14,14 @@ public class SampleClass {
      */
     public static void sayHelloName(String value) {
         System.out.println("こんにちは" + value + "さん");
+    }
+
+    /**
+     * @param String name 表示する名前
+     * @param int age 年齢
+     */
+    public static void sayNameAndAge(String name, int age) {
+        System.out.println("名前:" + name);
+        System.out.println("年齢:" + age);
     }
 }
