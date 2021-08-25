f2ecd59f340265bfb0cec541dbccacdd1ef7c768
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-25 13:23:36 +0900

    3.15.1 引数なし、戻り値なしのパターン。

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,5 +5,28 @@ public class HelloWorld {
         System.out.println("name: 山田 太郎");
         System.out.println("鳥取県");
         System.out.println("Tottori");
+
+//        int[] nums = {10, 20, 30};
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.println(nums[i]);
+//        }
+
+//        int[] nums = {10, 20, 30};
+//        for (int value: nums) {
+//            System.out.println(value);
+//        }
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(
+                   String.format(
+                       "%s x %s = %2s",
+                       i,
+                       j,
+                       i * j
+                   )
+                );
+            }
+        }
     }
 }
