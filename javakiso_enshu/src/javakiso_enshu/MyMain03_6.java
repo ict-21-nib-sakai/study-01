b239774dcb2b4be869a8baa2d3c182418111c44d
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-25 14:35:12 +0900

    演習 3.6 基本文法の確認（６） 引数なし、戻り値なしのメソッドの定義と利用

---
+++ javakiso_enshu/src/javakiso_enshu/MyMain03_6.java
@@ -0,0 +1,21 @@
+package javakiso_enshu;
+
+public class MyMain03_6 {
+    public static void main(String[] args) {
+        car();
+    }
+
+    /**
+     * 演習3.6 引数なし、戻り値なしの確認 (6)
+     */
+    public static void car() {
+        System.out.println(
+            "car メソッドを呼び出します" + "\n"
+            + "出発します。" + "\n"
+            + "前進します" + "\n"
+            + "後退します" + "\n"
+            + "止まります" + "\n"
+            + "car メソッドの呼び出しが完了しました"
+        );
+    }
+}
