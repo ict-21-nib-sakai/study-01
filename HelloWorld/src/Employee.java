e50d0d2daf5a35a63c83fbe4902b3ecc535dd26b
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-26 11:25:53 +0900

    従業員クラスのフィールドをカプセル化。

--- HelloWorld/src/Employee.java
+++ HelloWorld/src/Employee.java
@@ -3,19 +3,38 @@
  */
 public class Employee {
     /** 氏名 */
-    String name;
+    private String name;
 
     /** 年齢 */
-    int age;
+    private int age;
 
     /** 住所 */
-    String address;
+    private String address;
 
     /** 性別 */
-    int gender;
+    private int gender;
 
     /** 役職 */
-    String position;
+    private String position;
+
+    /**
+     * 従業員の氏名を代入
+     *
+     * @param String name
+     * @return this
+     */
+    public Employee setName(String name) {
+        this.name = name;
+
+        return this;
+    }
+
+    /**
+     * @return String 従業員の氏名
+     */
+    public String getName() {
+        return this.name;
+    }
 
     /**
      * 入社
