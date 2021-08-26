afdba36c80ce1dc4f0334d3dab587de0285a7db6
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-26 15:24:30 +0900

    コメントアウトしていて、未使用な部分を取り除いた。

--- javakiso_enshu/src/javakiso_enshu/MyMain04_2.java
+++ javakiso_enshu/src/javakiso_enshu/MyMain04_2.java
@@ -4,12 +4,10 @@ public class MyMain04_2 {
     public static void main(String[] args) {
         // 東京店のインスタンスを作成して、売上を表示
         Store storeTokyo = new Store("東京店", 500000);
-//        storeTokyo.print();
         storeTokyo.explain();
 
         // 渋谷店のインスタンスを作成して、売上を表示
         Store storeShibuya = new Store("渋谷店", 1000000);
-//        storeShibuya.print();
         storeShibuya.explain();
 
         // 全店舗の総売り上げを表示
