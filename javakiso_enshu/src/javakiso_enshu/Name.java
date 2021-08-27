4d1c1b8d9850149eb454d74a6a13a9a353087e89
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-27 11:06:53 +0900

    演習 6.1 クラスのカプセル化 Nameクラス

---
+++ javakiso_enshu/src/javakiso_enshu/Name.java
@@ -0,0 +1,14 @@
+package javakiso_enshu;
+
+public class Name {
+    /** 人名 */
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
