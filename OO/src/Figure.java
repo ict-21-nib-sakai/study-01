67432d2bf7a939cadfb2233d04934d157f890b24
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-30 16:28:53 +0900

    図形の抽象クラス。

---
+++ OO/src/Figure.java
@@ -0,0 +1,11 @@
+/**
+ * さまざまな図形に共通する属性・性質を持つクラス
+ */
+public abstract class Figure {
+    private String color;
+
+    /**
+     * 図形の面積を計算する抽象メソッド
+     */
+    public abstract double getArea();
+}
