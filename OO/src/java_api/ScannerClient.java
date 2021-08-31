e49c35faf3e8a34037bf792ae1884d0ef933235e
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-31 16:50:28 +0900

    ScannerClient クラスのソースコードを散らかしてしまったのでもとに戻す。
    @see コミットID
    1aaca35b55faf73b3c511be6b8a9da00480c2cca

--- OO/src/java_api/ScannerClient.java
+++ OO/src/java_api/ScannerClient.java
@@ -4,13 +4,9 @@ import java.util.Scanner;
 
 public class ScannerClient {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        String input = scanner.nextLine();
-//
-//        System.out.println("入力した文字列: " + input);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
-        // 文字列を整数に変換する
-        int data2 = Integer.parseInt("123");
-        System.out.println(data2);
+        System.out.println("入力した文字列: " + input);
     }
 }
