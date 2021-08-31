ac199bdaf86e68fddeff970bf2d70c8e459cf744
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-31 16:33:33 +0900

    文字列の比較。

--- OO/src/java_api/ScannerClient.java
+++ OO/src/java_api/ScannerClient.java
@@ -10,23 +10,24 @@ public class ScannerClient {
 //        System.out.println("入力した文字列: " + input);
 
 
-        String str1 = "Hello";
-        String str2 = "Hello";
-
-        // str1 と str2 の文字列が同じかどうかを調べる
-        if (str1 == str2) {
-            System.out.println("str1 と str2 は同じ");
-        } else {
-            System.out.println("str1 と str2 は違う");
-        }
-
-//        String str3 = new String("Hello");
-//        String str4 = "Hello";
+//        String str1 = "Hello";
+//        String str2 = "Hello";
 //
-//        if (str3 == str4) {
-//            System.out.println("str3 と str4 は同じ");
+//        // str1 と str2 の文字列が同じかどうかを調べる
+//        if (str1 == str2) {
+//            System.out.println("str1 と str2 は同じ");
 //        } else {
-//            System.out.println("str3 と str4 は違う");
+//            System.out.println("str1 と str2 は違う");
 //        }
+
+        // この書き方はアドレスが違うので、条件分岐は false と評価される。
+        String str3 = new String("Hello");
+        String str4 = "Hello";
+
+        if (str3 == str4) {
+            System.out.println("str3 と str4 は同じ");
+        } else {
+            System.out.println("str3 と str4 は違う");
+        }
     }
 }
