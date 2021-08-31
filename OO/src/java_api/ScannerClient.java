19b31f69ecf4087f37d230c8a0acda9f8653fb65
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-31 16:32:34 +0900

    文字列の比較。これは Java コンパイラが気を利かせて同じアドレスを参照しているので、意図せず true と評価されている。
    講師の先生の解説
    //以下のように記述した場合、
    
     //String str1 = "Hello";
    //String str2 = "Hello";
    
     //"Hello"を持つStringインスタンスがJavaによって一個だけつくられ、
    //その参照値を、str1、str2それぞれに格納。
    
     //同じ"Hello"という値なのでインスタンスを別個に持つ
    //のは無駄なので、このような動きになっている
    
     //２つの参照型変数str1とstr2の参照値は全く同じ
    //String str1 = "Hello";
    //String str2 = "Hello";
    
     //if(str1 == str2)・・・この条件式は、２つの参照型変数の参照値が
    //等しいかを調べている

--- OO/src/java_api/ScannerClient.java
+++ OO/src/java_api/ScannerClient.java
@@ -4,9 +4,29 @@ import java.util.Scanner;
 
 public class ScannerClient {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//
+//        System.out.println("入力した文字列: " + input);
 
-        System.out.println("入力した文字列: " + input);
+
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        // str1 と str2 の文字列が同じかどうかを調べる
+        if (str1 == str2) {
+            System.out.println("str1 と str2 は同じ");
+        } else {
+            System.out.println("str1 と str2 は違う");
+        }
+
+//        String str3 = new String("Hello");
+//        String str4 = "Hello";
+//
+//        if (str3 == str4) {
+//            System.out.println("str3 と str4 は同じ");
+//        } else {
+//            System.out.println("str3 と str4 は違う");
+//        }
     }
 }
