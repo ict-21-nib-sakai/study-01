f5055ec7fc8ecc04b63c90dfabbbf32dea219d55
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-08-31 16:42:39 +0900

    文字列を整数に変換する。

--- OO/src/java_api/ScannerClient.java
+++ OO/src/java_api/ScannerClient.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class ScannerClient {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//
+//        System.out.println("入力した文字列: " + input);
 
-        System.out.println("入力した文字列: " + input);
+        // 文字列を整数に変換する
+        int data2 = Integer.parseInt("123");
+        System.out.println(data2);
     }
 }
