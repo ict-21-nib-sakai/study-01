eadfb5557ef57d5657872d61aa2e67ecfa9e0aa1
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-09-01 14:14:22 +0900

    統合開発環境 復習プリント 演習21 入力回数「3」をハードコーディングせず、定数に用意した。

--- IntegratedDevelopmentEnvironment/src/enshu21/MyMain.java
+++ IntegratedDevelopmentEnvironment/src/enshu21/MyMain.java
@@ -9,7 +9,10 @@ import java.util.Scanner;
  */
 public class MyMain {
     public static void main(String[] args) {
-        System.out.println("商品名を3回入力してください。");
+        /** 入力回数 */
+        final int input_times = 3;
+
+        System.out.println("商品名を" + input_times + "回入力してください。");
 
         /** キーボードから入力した値を取得するScannerインスタンス */
         Scanner scanner = new Scanner(System.in);
@@ -17,15 +20,15 @@ public class MyMain {
         /** キーボードから入力された値を保持しておくリスト */
         List<String> list = new ArrayList<>();
 
-        // キーボードから入力を3回受け付ける
-        for (int i = 0; i < 3; i++) {
+        // キーボードから入力を所定の回受け付ける
+        for (int i = 0; i < input_times; i++) {
             System.out.printf("%dつ目の商品名: ", i + 1);
             String input = scanner.nextLine();
             list.add(input);
         }
 
         // 入力された商品名を表示する
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < input_times; i++) {
             System.out.printf(
                 "%dつ目の商品名: %s" + "\n",
                 i + 1,
