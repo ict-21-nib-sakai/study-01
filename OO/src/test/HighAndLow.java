1e04b77985ca72874b25a27174f1416e12be2560
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-09-01 16:54:35 +0900

    統合開発環境_テキスト 12.6 不具合を修正しサイトJUnitテストを実行。
    単体テストの結果、間違っているソースコードを訂正。

--- OO/src/test/HighAndLow.java
+++ OO/src/test/HighAndLow.java
@@ -10,7 +10,7 @@ public class HighAndLow {
     public String check(int v) {
         if (v == this.value) {
             return "draw";
-        } else if (v > this.value) { // 不具合を修正
+        } else if (v < this.value) {
             return "high";
         } else {
             return "low";
