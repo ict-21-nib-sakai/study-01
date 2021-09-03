3164d22b3b97d8e40f82ee9eb72c09045e27c600
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-09-03 16:38:11 +0900

    演習 4.2 授業員情報の登録 (複数件の登録) static main メソッドで授業員を2名登録した。

---
+++ database_access/src/enshu04_2/MyMain.java
@@ -0,0 +1,58 @@
+package enshu04_2;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class MyMain {
+    public static void main(String[] args) {
+        // JDBCドライバの読み込み
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        final String url = "jdbc:postgresql://localhost/jdbc";
+        final String user = "jdbc";
+        final String password = "jdbc";
+
+        /** 2名の従業員を登録する */
+        Employee[] employees = {
+            new Employee(2001, "Hanako SATO", 3),
+            new Employee(2002, "Kakuei TANAKA", 4)
+        };
+
+        // データベースへの接続
+        try (Connection con = DriverManager.getConnection(url, user, password)) {
+            final String sql =
+                "INSERT INTO employee (" +
+                    "  emp_id" +
+                    ", emp_name" +
+                    ", dept_id" +
+                    ", registered_date" +
+                    ") " +
+                    "VALUES (?, ?, ?, ?)";
+
+            /** SQL 実行オブジェクト */
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            LocalDateTime now = LocalDateTime.now();
+            Timestamp time = Timestamp.valueOf(now);
+
+            // 「?」を置換
+            for (Employee employee : employees) {
+                ps.setInt(1, employee.getEmp_id());
+                ps.setString(2, employee.getEmp_name());
+                ps.setInt(3, employee.getDept_id());
+                ps.setTimestamp(4, employee.getRegistered_date());
+
+                // SQL (INSERT) を実行
+                ps.executeUpdate();
+            }
+
+            System.out.println("レコードの登録が完了しました。");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
