5b4ced4eb7736e70bdafa122b8ce3af90ae78267
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-09-03 14:01:18 +0900

    演習 3.3 従業員の取得と表示 (特定の従業員を表示)

---
+++ database_access/src/enshu03_3/MyMain.java
@@ -0,0 +1,54 @@
+package enshu03_3;
+
+import java.sql.*;
+
+public class MyMain {
+    public static void main(String[] args) {
+        // JDBCドライバの読み込み
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        final String url = "jdbc:postgresql://localhost/jdbc";
+        final String user = "jdbc";
+        final String password = "jdbc";
+
+        // データベースへの接続
+        try (Connection con = DriverManager.getConnection(url, user, password)) {
+            final String sql =
+                "SELECT " +
+                    "  emp_id" +
+                    ", emp_name" +
+                    ", department.dept_name" +
+                    ", registered_date" +
+                    " FROM employee" +
+                    " INNER JOIN department" +
+                    " USING (dept_id)" +
+                    " WHERE emp_id = ?";
+
+            /** SQL 実行オブジェクト */
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            // 「?」を置換
+            ps.setInt(1, 1003);
+
+            /** SQL (SELECT) の実行結果の結果セット */
+            ResultSet rs = ps.executeQuery();
+
+            // 結果セットから1行ずつ、1カラムずつ取得して画面に出力
+            while (rs.next()) {
+                System.out.printf(
+                    "emp_id: %d | emp_name: %s | dept_name: %s | registered_date: %s" + "\n",
+                    rs.getInt("emp_id"),
+                    rs.getString("emp_name"),
+                    rs.getString("dept_name"),
+                    rs.getTimestamp("registered_date")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
