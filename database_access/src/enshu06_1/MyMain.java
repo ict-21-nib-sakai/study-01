464fd1b3829a4275f82b0eee7088e77989b9afe5
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-09-03 13:45:54 +0900

    演習3.1 ソースコードを書いた。

---
+++ database_access/src/enshu06_1/MyMain.java
@@ -0,0 +1,42 @@
+package enshu06_1;
+
+import java.sql.*;
+
+public class MyMain {
+    public static void main(String[] args) {
+        // JDBCドライバの読み込み
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        final String url = "jdbc:postgresql://localhost/jdbc";
+        final String user = "jdbc";
+        final String password = "jdbc";
+
+        // データベースへの接続
+        try (Connection con = DriverManager.getConnection(url, user, password)) {
+            final String sql = "SELECT * FROM employee";
+
+            /** SQL 実行オブジェクト */
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            /** SQL (SELECT) の実行結果の結果セット */
+            ResultSet rs = ps.executeQuery();
+
+            // 結果セットから1行ずつ、1カラムずつ取得して画面に出力
+            while (rs.next()) {
+                System.out.printf(
+                    "emp_id: %d | emp_name: %s | dept_id: %d | registered_date: %s" + "\n",
+                    rs.getInt("emp_id"),
+                    rs.getString("emp_name"),
+                    rs.getInt("dept_id"),
+                    rs.getTimestamp("registered_date")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
