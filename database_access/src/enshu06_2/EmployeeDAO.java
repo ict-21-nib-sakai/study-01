6692d9bfd562f2eafecea2a84b2943ffc9dab599
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-06 16:41:20 +0900

    データベースアクセス 演習問題 演習 6.2 findById の確認用。

--- database_access/src/enshu06_2/EmployeeDAO.java
+++ database_access/src/enshu06_2/EmployeeDAO.java
@@ -69,6 +69,49 @@ public class EmployeeDAO {
     }
 
     /**
+     * 指定した主キーを持つ従業員を返す
+     *
+     * @param emp_id 授業員ID
+     */
+    public Employee findById(int emp_id) throws SQLException {
+        Connection con = EmployeeDAO.Connect();
+
+        final String sql =
+            "SELECT *" +
+            " FROM employee" +
+            " INNER JOIN department" +
+            " ON employee.dept_id = department.dept_id" +
+            " WHERE emp_id = ? "
+        ;
+
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, emp_id);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (!rs.next()) {
+            return null;
+        }
+
+        Employee employee = new Employee();
+        employee
+            .setEmp_id(rs.getInt("emp_id"))
+            .setEmp_name(rs.getString("emp_name"))
+            .setDept_id(rs.getInt("dept_id"))
+            .setRegistered_date(rs.getTimestamp("registered_date"))
+        ;
+
+        Department department = new Department();
+        department
+            .setDept_id(rs.getInt("dept_id"))
+            .setDept_name(rs.getString("dept_name"));
+
+        employee.setDepartment(department);
+
+        return employee;
+    }
+
+    /**
      * @return データベース接続文字列
      */
     private static String JdbcUrl() {
@@ -76,6 +119,17 @@ public class EmployeeDAO {
             "jdbc:postgresql://%s:%d/jdbc",
             DB_HOST,
             DB_PORT
+        );
+    }
+
+    /**
+     * データベースに接続
+     */
+    private static Connection Connect() throws SQLException {
+        return DriverManager.getConnection(
+            EmployeeDAO.JdbcUrl(),
+            DB_USER,
+            DB_PASSWORD
         );
     }
 }
