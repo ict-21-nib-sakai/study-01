8968129d755aca4bf72e9f6705d2d50503496301
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-06 21:22:27 +0900

    データベースアクセス 演習問題 演習 6.2 findAll の実行確認

--- database_access/src/enshu06_2/Client.java
+++ database_access/src/enshu06_2/Client.java
@@ -1,6 +1,7 @@
 package enshu06_2;
 
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * データベースアクセス 演習問題
@@ -8,7 +9,7 @@ import java.sql.SQLException;
  */
 public class Client {
     public static void main(String[] args) {
-        EmployeeDAO dao = new EmployeeDAO();
+        final EmployeeDAO dao = new EmployeeDAO();
 
         //<editor-fold desc="findById の実行確認">
         label: try {
@@ -24,6 +25,21 @@ public class Client {
             System.out.println("dept_name: " + employee.getDepartment().getDept_name());
             System.out.println("registered_date: " + employee.getRegistered_date());
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        //</editor-fold>
+
+        //<editor-fold desc="findAll の実行確認">
+        try {
+            List<Employee> employees = dao.findAll();
+            for (Employee employee : employees) {
+                System.out.println("emp_id: " + employee.getEmp_id());
+                System.out.println("emp_name: " + employee.getEmp_name());
+                System.out.println("dept_id: " + employee.getDept_id());
+                System.out.println("dept_name: " + employee.getDepartment().getDept_name());
+                System.out.println("registered_date: " + employee.getRegistered_date());
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
