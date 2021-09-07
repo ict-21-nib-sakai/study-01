fd6bb93017c874e78f4270305a5dbd3d8efb64c9
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-07 13:32:22 +0900

    Eclipse は自動で書いてくれないけど @Override を書いておく。

--- WebSample/src/web/MyServlet.java
+++ WebSample/src/web/MyServlet.java
@@ -27,6 +27,7 @@ public class MyServlet extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
 //        response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -38,6 +39,7 @@ public class MyServlet extends HttpServlet {
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
 //        doGet(request, response);
