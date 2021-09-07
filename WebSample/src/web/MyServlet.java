cb43174bd39c03664128c99e3e99ac79998247ea
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-07 14:24:58 +0900

    日本語に対応

--- WebSample/src/web/MyServlet.java
+++ WebSample/src/web/MyServlet.java
@@ -1,13 +1,12 @@
 package web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class MyServlet
@@ -32,8 +31,10 @@ public class MyServlet extends HttpServlet {
         // TODO Auto-generated method stub
 //        response.getWriter().append("Served at: ").append(request.getContextPath());
 
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter writer = response.getWriter();
         writer.write("Jane Doe");
+        writer.write("浦島 太郎");
     }
 
     /**
