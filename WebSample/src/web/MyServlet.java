5c5edcc21d1aae40b5bf7c510dfbb35d5da2392f
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-07 11:39:34 +0900

    コメントアウト。

--- WebSample/src/web/MyServlet.java
+++ WebSample/src/web/MyServlet.java
@@ -28,7 +28,7 @@ public class MyServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+//        response.getWriter().append("Served at: ").append(request.getContextPath());
     }
 
     /**
@@ -36,7 +36,7 @@ public class MyServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        doGet(request, response);
+//        doGet(request, response);
     }
 
 }
