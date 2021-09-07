362387a7c926fc21d118d0d19984215f0f0217bc
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-07 09:26:34 +0900

    [WIP] データベースアクセス 演習問題 演習 6.2 insert を書いた。

--- database_access/src/enshu06_2/EmployeeDAO.java
+++ database_access/src/enshu06_2/EmployeeDAO.java
@@ -1,6 +1,7 @@
 package enshu06_2;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,6 +56,68 @@ public class EmployeeDAO extends DAO {
         }
 
         return employees;
+    }
+
+    /**
+     * 1件から複数の従業員を登録する
+     */
+    public List<Employee> insert(List<Employee> employees) throws SQLException {
+        final Connection con;
+
+        label:
+        try {
+            con = EmployeeDAO.Connect();
+
+            if (!con.isClosed()) {
+                break label;
+            }
+        } catch (SQLException e) {
+            // データベース接続失敗
+            throw e;
+        }
+
+        final List<Employee> insertedEmployee = new ArrayList<>();
+
+        try {
+            // 自動コミットモードを無効にする。
+            con.setAutoCommit(false);
+
+            // SQL の実行準備をする。
+            final PreparedStatement ps = con.prepareStatement(
+                "INSERT INTO employee VALUES (?, ?, ?, ?)"
+            );
+
+            final LocalDateTime now = LocalDateTime.now();
+            final java.sql.Timestamp time = java.sql.Timestamp.valueOf(now);
+
+            // 引数に渡された複数の従業員を1件ずつ employee テーブルに追加する。
+            for (Employee employee : employees) {
+                ps.setInt(1, employee.getEmp_id());
+                ps.setString(2, employee.getEmp_name());
+                ps.setInt(3, employee.getDept_id());
+                ps.setTimestamp(4, time);
+
+                // INSERT を実行
+                ps.executeUpdate();
+
+                /** INSERT の結果、登録された従業員 */
+                final Employee saved = this.findById(
+                    employee.getEmp_id()
+                );
+
+                // 戻り値用のリスト変数にINSERTの結果を追加する
+                insertedEmployee.add(saved);
+            }
+
+            // すべてうまく登録できたので、トランザクションをコミットする。
+            con.commit();
+        } catch (SQLException e) {
+            // 1件でも失敗したらロールバックする。
+            con.rollback();
+            throw e;
+        }
+
+        return insertedEmployee;
     }
 
     /**
