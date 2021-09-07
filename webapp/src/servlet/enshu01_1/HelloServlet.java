9ec6b33cc008ead651ecb5b5e51acc6c69ac7516
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-07 15:14:00 +0900

    Webアプリケーション開発 演習問題 演習 1.1 サーブレットによるWebページの表示
    Eclipse が自動作成して、都合のよくないソースコードを取り除いた。先生いわく、わるさするとのこと。

--- webapp/src/servlet/enshu01_1/HelloServlet.java
+++ webapp/src/servlet/enshu01_1/HelloServlet.java
@@ -30,7 +30,6 @@ public class HelloServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-//        response.getWriter().append("Served at: ").append(request.getContextPath());
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter writer = response.getWriter();
         writer.write("こんにちは！");
