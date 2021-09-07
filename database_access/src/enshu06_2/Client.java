47939aaaf896b117bf5f026c28976b6bac07430b
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-07 09:27:11 +0900

    データベースアクセス 演習問題 演習 6.2 insert の実行確認

--- database_access/src/enshu06_2/Client.java
+++ database_access/src/enshu06_2/Client.java
@@ -1,6 +1,7 @@
 package enshu06_2;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,6 +44,46 @@ public class Client {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        //</editor-fold>
+
+        //<editor-fold desc="insert の実行確認">
+        final List<Employee> employees = new ArrayList<>();
+
+        // 従業員1人目
+        final Employee employee1 = new Employee();
+        employee1
+            .setEmp_id(3001)
+            .setEmp_name("Taro YAMADA")
+            .setDept_id(1)
+        ;
+        employees.add(employee1);
+
+        // 従業員2人目
+        final Employee employee2 = new Employee();
+        employee2
+            .setEmp_id(3002)
+            .setEmp_name("Hanako TANAKA")
+            .setDept_id(2)
+        ;
+        employees.add(employee2);
+
+        // 複数の従業員を登録
+        try {
+            /** 登録された従業員 */
+            List<Employee> insertedEmployees = dao.insert(employees);
+
+            for (Employee insertedEmployee : insertedEmployees) {
+                System.out.println("emp_id: " + insertedEmployee.getEmp_id());
+                System.out.println("emp_name: " + insertedEmployee.getEmp_name());
+                System.out.println("dept_id: " + insertedEmployee.getDept_id());
+                System.out.println("dept_name: " + insertedEmployee.getDepartment().getDept_name());
+                System.out.println("registered_date: " + insertedEmployee.getRegistered_date());
+                System.out.println("-------------------------");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         //</editor-fold>
     }
 }
