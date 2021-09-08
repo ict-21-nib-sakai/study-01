7db2a997b06f1aa951ff32da9c1e490a89fa9c03
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-09-08 16:16:58 +0900

    Webアプリケーション開発 テキスト 7.3.2 フォワードによる連携。

---
+++ WebSample/src/collabo/CollaboServlet.java
@@ -0,0 +1,49 @@
+package collabo;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CollaboServlet
+ */
+@WebServlet("/CollaboServlet")
+public class CollaboServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CollaboServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /** 転送先のJSPのパス */
+        String path = "/collabo.jsp";
+
+        /** 転送用オブジェクト */
+        RequestDispatcher dip = request.getRequestDispatcher(path);
+
+        // 指定したPathに転送
+        dip.forward(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
