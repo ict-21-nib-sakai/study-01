844eac6e4b59e408ef48f5ab780cb118a426a64d
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-09-10 13:00:54 +0900

    9.1.1.3 リクエストスコープとEL式
    まずは、三角形を表す Triangle クラスを作った。

---
+++ WebSample/src/Triangle.java
@@ -0,0 +1,28 @@
+/**
+ * 三角形
+ */
+public class Triangle {
+    /** 三角形の底辺の長さ */
+    private int base;
+
+    /** 三角形の高さ */
+    private int height;
+
+    public int getBase() {
+        return base;
+    }
+
+    public Triangle setBase(int base) {
+        this.base = base;
+        return this;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Triangle setHeight(int height) {
+        this.height = height;
+        return this;
+    }
+}
