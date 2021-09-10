c2d8b55101d979eff07a5e826d4b0ff0e1a773c3
👩‍💻Kiyonori SAKAI👩‍💻 <ict21sakai@outlook.jp>
2021-09-10 16:02:37 +0900

    演習 9.3 EL式とJSTLの利用 (従業員クラス)

---
+++ webapp/src/servlet/enshu09_3/Employee.java
@@ -0,0 +1,50 @@
+package servlet.enshu09_3;
+
+import javax.naming.Name;
+
+/**
+ * 演習 9.3 EL式とJSTLの利用 (c:forEach タグ)
+ */
+public class Employee {
+    /** 従業員ID */
+    private int empId;
+
+    /** 従業員名 */
+    private String empName;
+
+    /** 部署名 */
+    private String deptName;
+
+    Employee(int empId, String empName, String deptName) {
+        this.empId = empId;
+        this.empName = empName;
+        this.deptName = deptName;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public Employee setEmpId(int empId) {
+        this.empId = empId;
+        return this;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public Employee setEmpName(String empName) {
+        this.empName = empName;
+        return this;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public Employee setDeptName(String deptName) {
+        this.deptName = deptName;
+        return this;
+    }
+}
