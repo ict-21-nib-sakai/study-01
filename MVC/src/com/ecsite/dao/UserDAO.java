28813aa224b3252a7b7d582d49787c402e0af4f3
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-13 13:14:13 +0900

    MVC プロジェクト users テーブルの操作をする DAO クラスを書いた。

---
+++ MVC/src/com/ecsite/dao/UserDAO.java
@@ -0,0 +1,44 @@
+package com.ecsite.dao;
+
+import com.ecsite.data.User;
+
+import java.sql.*;
+
+public class UserDAO extends CommonDAO {
+    public User selectByUserId(String userId) {
+        User user = null;
+
+        try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            final String sql =
+                "SELECT *" +
+                    " FROM users" +
+                    " WHERE user_id = ?" +
+                    " ;";
+
+            try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+
+                if (!rs.next()) {
+                    rs.close();
+                    con.close();
+                    return null;
+                }
+
+                user = new User();
+                user
+                    .setUserid(rs.getString("user_id"))
+                    .setName(rs.getString("name"))
+                    .setPassword(rs.getString("password"))
+                    .setAddress(rs.getString("address"))
+                ;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+}
