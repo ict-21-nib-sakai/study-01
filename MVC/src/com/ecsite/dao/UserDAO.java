9e178251190294feeed69ec59c8ec395d00dccbe
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-13 15:12:37 +0900

    MVC プロジェクト PreparedStatement にて、値を代入し忘れていた。

--- MVC/src/com/ecsite/dao/UserDAO.java
+++ MVC/src/com/ecsite/dao/UserDAO.java
@@ -16,6 +16,7 @@ public class UserDAO extends CommonDAO {
                     " ;";
 
             try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setString(1, userId);
                 ResultSet rs = ps.executeQuery();
 
                 if (!rs.next()) {
