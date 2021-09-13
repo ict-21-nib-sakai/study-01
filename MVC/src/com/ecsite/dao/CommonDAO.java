70f9543677aa193d224af9ddd17012e98923032b
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-13 13:12:04 +0900

    モデルのスーパークラスを書いた。

---
+++ MVC/src/com/ecsite/dao/CommonDAO.java
@@ -0,0 +1,10 @@
+package com.ecsite.dao;
+
+/**
+ * DAOの共通クラス
+ */
+public class CommonDAO {
+    protected static final String URL = "jdbc:postgresql://localhost/ecsite";
+    protected static final String USER = "postgres";
+    protected static final String PASSWORD = "postgres";
+}
