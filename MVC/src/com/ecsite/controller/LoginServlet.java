f4460df20f2cc8bdd41d2ecf043ca2828702a401
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-13 13:59:56 +0900

    MVC プロジェクト ログイン後に、商品一覧を表示するように書いた。

--- MVC/src/com/ecsite/controller/LoginServlet.java
+++ MVC/src/com/ecsite/controller/LoginServlet.java
@@ -1,6 +1,8 @@
 package com.ecsite.controller;
 
+import com.ecsite.data.Item;
 import com.ecsite.data.User;
+import com.ecsite.service.ItemService;
 import com.ecsite.service.LoginService;
 
 import javax.servlet.ServletException;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Servlet implementation class LoginServlet
@@ -38,14 +41,26 @@ public class LoginServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LoginService service = new LoginService();
-        User user = service.execute(request);
+        LoginService loginService = new LoginService();
+        User user = loginService.execute(request);
 
-        if (user != null) {
-            // 商品一覧を取得し、itemList.jsp に遷移
-        } else {
+        if (null == user) {
             // 認証失敗
             doGet(request, response);
+            return;
         }
+
+        // 商品一覧を取得し、itemList.jsp に遷移
+        ItemService itemService = new ItemService();
+        List<Item> itemList = itemService.findAll();
+
+        // ビューに値を渡す
+        request.setAttribute("itemList", itemList);
+
+        // ビューを表示
+        request
+            .getRequestDispatcher("/WEB-INF/itemList.jsp")
+            .forward(request, response)
+        ;
     }
 }
