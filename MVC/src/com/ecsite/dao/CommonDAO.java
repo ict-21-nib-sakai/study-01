478007a92f897888846efcedd50c427aea7b1ab0
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-13 15:11:51 +0900

    MVC プロジェクト PostgreSQL のポート番号を変更しているので、あわせてスーパークラスをの定数を訂正。

--- MVC/src/com/ecsite/dao/CommonDAO.java
+++ MVC/src/com/ecsite/dao/CommonDAO.java
@@ -4,7 +4,7 @@ package com.ecsite.dao;
  * DAOの共通クラス
  */
 public class CommonDAO {
-    protected static final String URL = "jdbc:postgresql://localhost/ecsite";
+    protected static final String URL = "jdbc:postgresql://localhost:15432/ecsite";
     protected static final String USER = "postgres";
     protected static final String PASSWORD = "postgres";
 }
