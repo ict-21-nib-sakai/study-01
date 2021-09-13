0cc85be45cb1030cea6ca96fdb25847ec1c2641d
🎋Kiyonori SAKAI🎋 <ict21sakai@outlook.jp>
2021-09-13 13:11:46 +0900

    MVC プロジェクト ログインのアクション関数を書いた。

---
+++ MVC/src/com/ecsite/controller/LoginServlet.java
@@ -0,0 +1,44 @@
+package com.ecsite.controller;
+
+import com.ecsite.data.User;
+import com.ecsite.service.LoginService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = "/WEB-INF/login.jsp";
+        request.getRequestDispatcher(path).forward(request, response);
+    }
+
+    /**
+     * ログイン処理
+     *
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LoginService service = new LoginService();
+        User user = service.execute(request);
+    }
+}
